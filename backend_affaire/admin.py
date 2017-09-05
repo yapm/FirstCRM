06f1d2c253946411c279ba94588280b593010f5e
yapm <alex.maia.utbm@gmail.com>
2017-09-05 23:44:25 +0200

    premier test pour comprendre le commit

--- backend_affaire/admin.py
+++ backend_affaire/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Seller, Bid
 
 admin.site.register(Product)
+admin.site.register(Bid)
+admin.site.register(Seller)
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'type_produit', 'date_creation')
--- backend_affaire/models.py
+++ backend_affaire/models.py
@@ -3,11 +3,31 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Product(models.Model):
-    name = models.CharField(max_length=50)
-    type_produit = models.CharField(max_length=70, null=True)
-    description = models.TextField(null=True)
-    date_creation = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date of creation")
+class Bid(models.Model):
+    affair_number = models.CharField(max_length=10, null=True, blank=True, unique=True)
+    project_price = models.DecimalField(max_digits=13, decimal_places=2, verbose_name="Price of the project", null = True)
+#    customer = models.ForeignKey('Customer', verbose_name = "Customer")
+#    final_customer = models.ForeignKey('Customer', verbose_name = "Final Customer")
+#    shipyard = models.ForeignKey('Customer', verbose_name = "Shipyard")
+#    seller = models.ForeignKey('Seller', verbose_name = "Seller")
+#    scope_supply = models.ForeignKey('ScopeSupply', verbose_name = "Scope of Supply")
+
+    def __str__(self):
+        """
+        permet de reconnaître les différents modèles dans l'administration
+        """
+        return self.affair_number
+
+    def __unicode__(self):
+       return self.affair_number
+
+class Person(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    surname = models.CharField(max_length=255, null=True, blank=True)
+    trigramme = models.CharField(max_length=4, null=True, blank=True)
+
+    class meta:
+        abstract = True
 
     def __str__(self):
         """
@@ -15,5 +35,37 @@ class Product(models.Model):
         """
         return self.name
 
-def __unicode__(self):
-    return self.name
+    def __unicode__(self):
+       return self.name
+
+class Seller(Person):
+   region_allocated =  models.CharField(max_length=255, null=True, blank=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    type_produit = models.CharField(max_length=70, null=True)
+    price_std = models.DecimalField(max_digits=15, decimal_places=2, verbose_name="Standard price", null = True)
+    ax_cost = models.DecimalField(max_digits=15, decimal_places=2, verbose_name="Cost in AX", null = True)
+    description = models.TextField(null=True)
+
+#    class meta:
+#        abstract = True
+#Cette base sert de skelette mais aussi pour définir les produits de base
+    def __str__(self):
+        """
+        permet de reconnaître les différents modèles dans l'administration
+        """
+        return self.name
+
+    def __unicode__(self):
+       return self.name
+
+class ProductProject(Product):
+    bid_project = models.ForeignKey('Bid', null = True)
+    cost_project = models.DecimalField(max_digits=15, decimal_places=2, verbose_name="Project cost", null = True)
+    price_quotation = models.DecimalField(max_digits=15, decimal_places=2, verbose_name="Price quotation", null = True)
+
+class CompositionNetans(ProductProject):
+    nb_mod_rs422 = models.PositiveSmallIntegerField(null = True)
+    nb_mod_synchro = models.PositiveSmallIntegerField(null = True)
+    nb_mod_ntds= models.PositiveSmallIntegerField(null = True)
